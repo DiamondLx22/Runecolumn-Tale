8aa8750f034fe715a835067a49183d68071b5efd
KatjaCharfreitag <Katja.Charfreitag@gmail.com>
2024-08-26 17:19:43 +0200

    Music Added & Menu

---
+++ Assets/Fantasy Tavern Music Pack/ChrisGruchacz_Fantasy_Tavern_Music_Pack/Music/menu.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class menu : MonoBehaviour{
+    public bool option_menu = false;
+    public GameObject option_screen;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CheckOptionScreen();
+    }
+    void CheckOptionScreen(){
+        if (option_menu==true){
+            option_screen.SetActive(true);
+        }else{
+            option_screen.SetActive(false);
+        }
+
+
+
+    }
+
+
+
+
+
+
+
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("ggg");
+     
+
+    }
+    public void QuitGame(){
+        Application.Quit();
+
+
+    }
+    public void Option(){
+        option_menu = true;
+    
+    }
+    public void CloseOptionscreen(){
+        option_menu = false;
+    
+    }
+}
+
