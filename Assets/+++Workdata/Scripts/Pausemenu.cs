b8eb7b3b9d33d83dd84ef690f1dd9ca1af53592c
RedWeasel1997 <137084646+RedWeasel1997@users.noreply.github.com>
2024-08-30 19:44:42 +0200

    InventoryBasics 30.08

--- Assets/+++Workdata/Scripts/Pausemenu.cs
+++ Assets/+++Workdata/Scripts/Pausemenu.cs
@@ -2,56 +2,69 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.InputSystem; // Importiert das neue Input System
 
 public class Pausemenu : MonoBehaviour
 {
     public GameObject pausemenu;
     public static bool isPaused;
-    // Start is called before the first frame update
+
+    // Reference to the Input Action Asset
+    private InputAction pauseAction;
+
     void Start()
     {
         pausemenu.SetActive(false);
+
+        // Initialisieren Sie die Input Actions
+        var inputActionAsset = new PauseMenuInputActions();
+        pauseAction = inputActionAsset.UI.Pause;
+        pauseAction.Enable();
+
+        // Fügen Sie den Callback für die Pause-Funktion hinzu
+        pauseAction.performed += OnPause;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnDestroy()
     {
-        if (Input.GetKeyDown(KeyCode.Escape))
+        // Entfernen Sie den Callback, wenn das Objekt zerstört wird
+        pauseAction.performed -= OnPause;
+    }
+
+    private void OnPause(InputAction.CallbackContext context)
+    {
+        if (isPaused)
         {
-            if (isPaused)
-            {
-                ResumeGame();
-
-            }
-            else
-            {
-                PauseGame();
-            }
-
-
-
+            ResumeGame();
+        }
+        else
+        {
+            PauseGame();
         }
     }
+
     public void PauseGame()
     {
         pausemenu.SetActive(true);
         Time.timeScale = 0f;
         isPaused = true;
     }
+
     public void ResumeGame()
     {
         pausemenu.SetActive(false);
         Time.timeScale = 1f;
         isPaused = false;
     }
+
     public void GoToMainMenu()
     {
         Time.timeScale = 1f;
         SceneManager.LoadScene("MainMenu");
-        }
-            public void QuitGame()
+    }
+
+    public void QuitGame()
     {
         Application.Quit();
-        
     }
 }
---
+++ Assets/StateManager.cs
@@ -0,0 +1,87 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class StateManager : MonoBehaviour
+{
+
+    private GameController gameController;
+    public StateInfo[] stateInfos;
+    //  public List<StateInfo> stateInfos;
+    [SerializeField] TextMeshProUGUI text_header, text_description;
+    [SerializeField] private GameObject stateContainer;
+    [SerializeField] private Image image;
+    [SerializeField] private Button button;
+
+    private void Awake()
+    {
+        gameController = FindObjectOfType<GameController>();
+    }
+
+    private void OnEnable()
+    {
+        GameState.StateAdded += NewStateCollected;
+    }
+
+
+    private void OnDisable()
+    {
+        GameState.StateAdded -= NewStateCollected;
+    }
+
+    void NewStateCollected(string id, int amount)
+    {
+        foreach (StateInfo stateInfo in stateInfos)
+        {
+            if (stateInfo.id == id)
+            {
+                text_header.SetText(stateInfo.itemName);
+                text_description.SetText(stateInfo.description);
+                image.sprite = stateInfo.icon;
+            }
+        }
+
+        //print($"new Item collected with the id: {id} with the amount of");
+        StartCoroutine(DelayOpenPanel());
+
+
+    }
+
+    IEnumerator DelayOpenPanel()
+    {
+        yield return null;
+        stateContainer.SetActive(true);
+        Selectable newSelection;
+        newSelection = button;
+
+        yield return null; // Wait for next Update() / next frame
+
+        newSelection.Select();
+        //gameController.StartStatePopUp();
+    }
+
+    public void CloseStatePopUp()
+    {
+        stateContainer.SetActive(false);
+        //EventSystem.current.SetSelectedGameObject(null);
+        //gameController.EndStatePopUpMode();
+    }
+
+    public StateInfo GetStateById(string id)
+    {
+        foreach (StateInfo stateInfo in stateInfos)
+        {
+            if (stateInfo.id == id)
+            {
+                return stateInfo;
+            }
+        }
+
+        return null;
+    }
+}
