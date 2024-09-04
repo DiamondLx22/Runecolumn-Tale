a3340d4299cc3ee972c72da6680757efb97f0da7
DiamondLx22 <167439792+DiamondLx22@users.noreply.github.com>
2024-09-04 17:29:22 +0200

    Enemy TakeDamage Changes

--- Assets/+++Workdata/Scripts/EnemyScripts/IDamageable.cs
+++ Assets/+++Workdata/Scripts/EnemyScripts/IDamageable.cs
@@ -4,8 +4,6 @@ using UnityEngine;
 
 public interface IDamageable
 {
-    float Health { set; get; }
-    void OnHit(Vector2 knockback);
     void OnHit(float damage);
     void OnHit(float damage, Vector2 knockback);
 }
