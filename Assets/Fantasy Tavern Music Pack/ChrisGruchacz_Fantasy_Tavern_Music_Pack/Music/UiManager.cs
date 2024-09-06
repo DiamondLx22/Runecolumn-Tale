6cdf3f196a0be6d13e8ff30c466b3e63cf6669af
KatjaCharfreitag <Katja.Charfreitag@gmail.com>
2024-09-06 15:02:13 +0200

    Fixed Optionscreen

---
+++ Assets/Fantasy Tavern Music Pack/ChrisGruchacz_Fantasy_Tavern_Music_Pack/Music/UiManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UiManager : MonoBehaviour
+{
+    public bool option_menu = false;
+
+    public GameObject option_screen;
+    public GameObject mainMenu;
+    private void OnEnable()
+    {
+
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("MainWorld");
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void CallOptionMenu()
+    {   
+        option_menu = !option_menu;
+        option_screen.SetActive(option_menu);
+        mainMenu.SetActive(!option_menu);
+
+    }
+}
+
